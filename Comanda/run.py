3ba4441ca2019694df4eac14b67d37db6e5b9d47
FBayinay <federicobayinay@gmail.com>
2024-06-17 02:31:07 -0300

    corrección

--- Comanda/run.py
+++ Comanda/run.py
@@ -4,5 +4,5 @@ import os
 
 if __name__ == '__main__':
     sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-    from visualization import visualization
-    visualization.main()
+    from comanda.login.login_ui import main as login_main
+    login_main()
