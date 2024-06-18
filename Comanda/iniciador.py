9acaf7aecb40cda67e872af3451f4928198df431
RodriJul <julianagusrodriguez931@gmail.com>
2024-06-18 17:21:00 -0300

    iniciador

---
+++ Comanda/iniciador.py
@@ -0,0 +1,15 @@
+# run_comanda.py
+import subprocess
+import sys
+
+def main():
+    try:
+        result = subprocess.run([sys.executable, '-m', 'comanda.run'], check=True)
+        print(f"Process finished with return code {result.returncode}")
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+if __name__ == '__main__':
+    main()
