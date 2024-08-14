9486d7f42d255be105483a6825ab6a2ab4ada895
FBayinay <federicobayinay@gmail.com>
2024-08-13 22:13:29 -0300

    listo

--- Restaurante/app/mapping/__init__.py
+++ Restaurante/app/mapping/__init__.py
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-from    action_schema        import  ActionSchema
-from    comand_schema        import  CommandSchema
-from    comanddeatil_schema  import  CommandDetailSchema
-from    login_shcema         import  LoginSchema
-from    menu_shcema          import  MenuSchema
-from    menu_category_shcema import  MenuCategorySchema
-from    menu_item_shcema     import  MenuItemSchema
-from    movemenr_shcema      import  WarehouseMovementSchema
-from    order_shcema         import  OrderSchema
-from    product_shcema       import  ProductSchema
-from    receipt_shcema       import  ReceiptSchema
-from    role_schema          import  RoleSchema
-from    stock_shcema         import  StockSchema
-from    supplier_shcema      import  SupplierSchema
-from    table_shcema         import  TableSchema
-from    user_shcema          import  UserSchema 
-
-
-
-=======
 from    .action_schema        import  ActionSchema
 from    .command_schema        import  CommandSchema
 from    .command_deatil_schema  import  CommandDetailSchema
@@ -36,4 +15,3 @@ from    .supplier_shcema      import  SupplierSchema
 from    .table_shcema         import  TableSchema
 from    .user_shcema          import  UserSchema
 from    .response_schema      import  ResponseSchema
->>>>>>> df70afd96dde4473e521c7970a5de58e11f20c65
